5713618e982082f1d4e3d5b7e8bed40a6e31218e
cofrela <cofrela@gmail.com>
2017-06-17 12:48:16 +0700

    css свойства

---
+++ Селекторы css/script.js
@@ -0,0 +1,10 @@
+
+function addStyle1() {
+    $("#st1").css("background", "red");
+}
+function addStyle2() {
+    $("p.withBorder").css("border", "1px solid blue");
+}
+function addStyle3() {
+    $("#st1, #st3").css("fontWeight", "bold");
+}
